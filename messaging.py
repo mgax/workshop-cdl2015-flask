edd06ee5f06d4f3e7ac8fa7c2cdb1c579e74b2e8
Alex Morega <alex@grep.ro>
2015-04-03 23:54:31 +0300

    message detail view

--- messaging.py
+++ messaging.py
@@ -27,5 +27,11 @@ def new():
     return flask.redirect(flask.url_for('home'))
 
 
+@app.route('/message/<int:message_id>')
+def message_view(message_id):
+    message = Message.query.get_or_404(message_id)
+    return flask.render_template('message.html', message=message)
+
+
 db.create_all()
 app.run(debug=True)
