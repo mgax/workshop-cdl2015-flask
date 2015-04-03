cf6708a3243b71008be5c59a7b6cba1cd4f56463
Alex Morega <alex@grep.ro>
2015-04-03 23:43:02 +0300

    show what the form submits

--- messaging.py
+++ messaging.py
@@ -8,4 +8,11 @@ def home():
     return flask.render_template('home.html')
 
 
+@app.route('/new', methods=['POST'])
+def new():
+    text = flask.request.form['message']
+    print text
+    return flask.redirect(flask.url_for('home'))
+
+
 app.run(debug=True)
